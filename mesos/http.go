5a3b583b29a557c498c374e03af732c48a5e92e4
Dominic Hamon <dhamon@twitter.com>
2014-07-20 23:08:14 -0700

    Switch to event loop model

--- mesos/http.go
+++ mesos/http.go
@@ -1,6 +1,7 @@
 package mesos
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -8,29 +9,24 @@ import (
 	"strings"
 )
 
-const (
-	port = 8888
-)
-
 var (
-//	port       = flag.Int("port", 4242, "Port to listen on for HTTP endpoint")
+	mesosPort	= flag.Int("mesos_port", 8888, "Port to listen on for mesos messages")
 )
 
-func init() {
-	http.HandleFunc("/", rootHandler)
+type Handler struct {}
+
+func Listen() {
 	httpWaitGroup.Add(1)
-	go startServing()
+	go func() {
+		log.Printf("framework listening on port %d", *mesosPort)
+		httpWaitGroup.Done()
+		if err := http.ListenAndServe(fmt.Sprintf(":%d", *mesosPort), &Handler{}); err != nil {
+			log.Fatalf("failed to start framework listening on port %d", *mesosPort)
+		}
+	}()
 }
 
-func startServing() {
-	log.Printf("listening on port %d", port)
-	httpWaitGroup.Done()
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
-		log.Fatalf("failed to start listening on port %d", port)
-	}
-}
-
-func rootHandler(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Add("Allow", "POST")
 		w.WriteHeader(http.StatusMethodNotAllowed)
